c51aa7a15f8a486283b797fc1fe45f98b7281605
John Clayton <john@mini.local>
2020-03-09 21:39:44 +0100

    Histograms, they act weird.

--- main.py
+++ main.py
@@ -1,25 +1,61 @@
-from prometheus_client import start_http_server, Histogram
+from prometheus_client import start_http_server, Histogram, Gauge
+import psycopg2
 import random
 import time
-
-_INF = float("inf")
-MINS_PER_DAY = 1440
-
-buckets = (30, 60, MINS_PER_DAY / 2, MINS_PER_DAY, MINS_PER_DAY * 7, MINS_PER_DAY * 30, _INF)
-observations = 10
+import sys
 
 # Create a metric to mimic check in times for client devices
-m = Histogram('device_checkin_time', 
-    'How long since a client last checked in, units are minutes, accurate to within the frequency that the script is executed', 
-    buckets=buckets)
+# m = Histogram('device_checkin_time', 
+#     'How long since a client last checked in, units are days, accurate to within the frequency that the script is executed', 
+#     buckets=(1, 7, 30, float("inf")))
 
-for b in range(1, observations):
-    m.observe(random.randint(1, b))
+client_checkin_duration_days = Gauge('device_checkin_days', 'the number of days elapsed since a device checked in', ["days",])
+
+collect_statement = "select u.name, u.id, i.last_check_in, now()::date - i.last_check_in::date how_old, " \
+        "dc.filewave_model_number, i.id from admin.user_clone_group u, public.generic_genericclient i, " \
+        "public.generic_genericdesktopclient dc where u.id = i.filewave_id and i.id = dc.genericclient_ptr_id"
+
+# this task will run every minute
+def collect_postgres_information():
+    try:
+        
+        conn = psycopg2.connect(host="localhost", port=9432, database="mdm", user="django")
+        cur = conn.cursor()
+        cur.execute(collect_statement)
+        rows = cur.fetchall()
+
+        # print("got %d rows" % (cur.rowcount))
+        buckets = [0, 0, 0, 0]
+        
+        for row in rows:
+            checkin_days = row[3]
+            if(checkin_days <= 1):
+                buckets[0] += 1
+            elif checkin_days <= 7:
+                buckets[1] += 1
+            elif checkin_days <= 30:
+                buckets[2] += 1
+            else:
+                buckets[3] += 1
+            client_checkin_duration_days.labels('Less than 1').set(buckets[0])
+            client_checkin_duration_days.labels('Less than 7').set(buckets[1])
+            client_checkin_duration_days.labels('Less than 30').set(buckets[2])
+            client_checkin_duration_days.labels('More than 30').set(buckets[3])
+        if conn is not None:
+            print("stats collected - used %d rows", (cur.rowcount,))
+            conn.close()
+    except Exception as reason: # for any reason...
+        print("Failed to collect info from the postgres database - aborting for now: ", reason)
 
 if __name__ == "__main__":
-    # Serve these stats...
+    print("Off we go, Ctrl-C to stop... ")
+    # Serve these stats... via glorius and wonderful http
     start_http_server(8000)
 
-    while(True):
-        m.observe(random.randint(1, MINS_PER_DAY * 40))
-        time.sleep(1)
+    try:
+        while(True):
+            collect_postgres_information()
+            time.sleep(30)
+    except:
+        print("Closing...")
+
