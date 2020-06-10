d5b96b538333f7960f1cf18c1026a9384ba8ea1a
Neil Clayton <neil@shinywhitebox.com>
2020-06-11 10:16:57 +1200

    Fixing extraction to handle \n, making it possible to run the tsdb import from a bunch of separate files.

--- filewave-backfill/backfill.py
+++ filewave-backfill/backfill.py
@@ -1,5 +1,7 @@
 import concurrent.futures
+import os
 import re
+import subprocess
 from datetime import datetime, timezone, timedelta
 from random import randint
 
@@ -8,17 +10,15 @@ import progressbar
 import requests
 
 
-def process_metric(duration: float, freq: int, callback, show_progress=False):
+def process_metric(duration: float, freq: int, callback):
     # Determine most historical timestamp; based on now - duration.
     # Step forward using freq interval. Generate random valued metric for given timestamp / metric name.
     end_time = datetime.now(timezone.utc)
     start_time = end_time - timedelta(days=duration)
-    bar = progressbar.ProgressBar(min_value=start_time.timestamp(), max_value=end_time.timestamp())
     while start_time < end_time:
         timestamp = start_time.timestamp() * 1e3
         callback(timestamp)
         start_time = start_time + timedelta(seconds=freq)
-        bar.update(start_time.timestamp())
 
 
 # TODO:
@@ -27,7 +27,7 @@ def process_metric(duration: float, freq: int, callback, show_progress=False):
 
 @click.group()
 def cli():
-    click.echo("Yo!")
+    click.echo("Importer of impending doom. Welcome. Please take a seat.")
 
 
 @cli.command(name='makedata')
@@ -57,7 +57,7 @@ def backfill_importer(metrics, output, freq, duration):
                 metric_line = f"{metric_name} {metric_value} {int(timestamp)}"
                 output_file.write(metric_line)
 
-            process_metric(duration, freq, write_me_please, False)
+            process_metric(duration, freq, write_me_please)
 
     output_file.write("# EOF")
 
@@ -69,14 +69,14 @@ class SkippedException(Exception):
     pass
 
 
-def get_value_for_key(dict, key, description: str):
-    if key not in dict:
+def get_value_for_key(some_dict, key, description: str):
+    if key not in some_dict:
         raise click.ClickException(f'Expected to see key {key} in {description} dictionary')
-    return dict[key]
+    return some_dict[key]
 
 
-def expect_value_for_key(dict, key, expected_value, description: str, error='Response not valid'):
-    value = get_value_for_key(dict, key, description)
+def expect_value_for_key(some_dict, key, expected_value, description: str):
+    value = get_value_for_key(some_dict, key, description)
     if value != expected_value:
         raise click.ClickException(f"Expected value for key {key} to equal '{expected_value}'. Got '{value}' instead")
     return value
@@ -89,9 +89,9 @@ def make_get_request_for_json(url):
     return r.json()
 
 
-def define_metric_for_this_url(metric_name: str, url, range: str):
+def define_metric_for_this_url(metric_name: str, url, prom_date_range: str):
     # click.echo(f"Finding labels for ... {metric_name}, range {range}")
-    endpoint = f"{url}/api/v1/query?query={metric_name}[{range}]"
+    endpoint = f"{url}/api/v1/query?query={metric_name}[{prom_date_range}]"
     json = make_get_request_for_json(endpoint)
 
     expect_value_for_key(json, "status", "success", f"response for label query for : {metric_name}")
@@ -131,38 +131,43 @@ def define_metric_for_this_url(metric_name: str, url, range: str):
 
 @cli.command(name='create')
 @click.option('-u', '--url', default="http://localhost:9090")
-@click.option('-o', '--output', default="backfill_from_existing.txt")  # output file; will be overwritten
-@click.option('-r', '--range', default="30m", help="How far to look back for values")
+@click.option('-m', '--metric', 'metrics', multiple=True, default=[], help="A metric name, eg: filewave_server_license_status. Can be specified multiple times")
+@click.option('-o', '--output', 'output_path', default="backfill_data", help='name of folder to use for created data')
+@click.option('-r', '--range', 'prom_date_range', default="30m", help="How far to look back for values")
 @click.option('-f', '--freq', default=15, help="frequency of samples", type=click.types.FLOAT)
 @click.option('-d', '--duration', default=1, help="duration of data to create", type=click.types.FLOAT)
 @click.option('-l', '--limit', help="If set, limit to the first N metrics. So dev doesn't take 100 years",
               type=click.types.INT)
 @click.option('-t', '--threads', 'num_threads', default=100, help="How much damage to do", type=click.types.INT)
-def create_from_existing(url, output, range: str, freq, duration, limit, num_threads):
-    endpoint = f"{url}/api/v1/label/__name__/values"
-    click.echo(f"Reading from {endpoint}")
-    r = requests.get(endpoint)
-    if not r.ok:
-        raise click.ClickException(f'Failed to talk to endpoint: {r.status_code}')
-    json_payload = r.json()
+def create_from_existing(url, metrics, output_path, prom_date_range: str, freq, duration, limit, num_threads):
+    if len(metrics) == 0:
+        endpoint = f"{url}/api/v1/label/__name__/values"
+        click.echo(f"Reading from {endpoint}")
+        r = requests.get(endpoint)
+        if not r.ok:
+            raise click.ClickException(f'Failed to talk to endpoint: {r.status_code}')
+        json_payload = r.json()
 
-    # Expect 'status' == success
-    expect_value_for_key(json_payload, "status", "success", "main response")
+        # Expect 'status' == success
+        expect_value_for_key(json_payload, "status", "success", "main response")
 
-    # Should use regex for this, and add as @click.option
-    all_the_metric_names = [n for n in json_payload["data"] if
-                            n not in ['ALERTS', 'ALERTS_FOR_STATE', 'up', 'redis_up']]
+        # Should use regex for this, and add as @click.option
+        all_the_metric_names = [n for n in json_payload["data"] if
+                                n not in ['ALERTS', 'ALERTS_FOR_STATE', 'up', 'redis_up']]
 
-    # Do the top 10
-    if limit:
-        all_the_metric_names = all_the_metric_names[:limit]
+        # Do the top 10
+        if limit:
+            all_the_metric_names = all_the_metric_names[:limit]
+    else:
+        all_the_metric_names = metrics
 
     metrics_dict = {}
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-        future_metric_definitions = {executor.submit(define_metric_for_this_url, metric_name, url, range): metric_name
-                                     for
-                                     metric_name in all_the_metric_names}
+        future_metric_definitions = {
+            executor.submit(define_metric_for_this_url, metric_name, url, prom_date_range): metric_name
+            for metric_name in all_the_metric_names
+        }
         for future in concurrent.futures.as_completed(future_metric_definitions):
             metric_name = future_metric_definitions[future]
             try:
@@ -172,22 +177,31 @@ def create_from_existing(url, output, range: str, freq, duration, limit, num_thr
 
     click.echo(f"Done. Defined {len(metrics_dict)} metrics from {url}")
     click.echo(f"Filling your SSD with text...")
-    output_file = open(output, 'w')
+
+    valid_label_regex = r"[a-zA-Z_][a-zA-Z0-9_]*"
+
+    if not os.path.exists(output_path):
+        click.echo(f"Making output folder: {output_path}")
+        os.makedirs(output_path)
 
     # Timestamps must be ascending, otherwise tsdb will crash with 'out of bounds'
-    def write_all_metrics_for_this_ts(timestamp):
-        valid_label_regex = r"[a-zA-Z_][a-zA-Z0-9_]*"
+    def write_single_file_for_metric(metric_name):
+        output_file_name = os.path.join(output_path, f"{metric_name}.txt")
+        output_file = open(output_file_name, 'w')
 
-        for metric_name in list(metrics_dict.keys()):
+        def write_all_metrics_for_this_ts(timestamp):
             definition = metrics_dict[metric_name]
             results_array = definition["results"]
 
-            # OK. So we have one or more results.
-            # Lets spit out REAL values, over the time period required
-            # We have one result per combination of labels. Each result has all the data for the range (from which we're going to pick one).
+            def fix_label_values(value):
+                return value.replace("\n", " ")
+
+            # OK. So we have one or more results. Lets spit out REAL values, over the time period required We have
+            # one result per combination of labels. Each result has all the data for the range (from which we're
+            # going to pick one).
             for result_for_some_labels in results_array:
                 labels_and_values = result_for_some_labels["metric"]
-                valid_labels = {label: val for label, val in labels_and_values.items() if
+                valid_labels = {label: fix_label_values(val) for label, val in labels_and_values.items() if
                                 label != "__name__" and re.match(valid_label_regex, label)}
                 if len(valid_labels.keys()) < len(labels_and_values.keys()) - 1:
                     click.echo(f"Ignored some labels for {metric_name}")
@@ -199,11 +213,37 @@ def create_from_existing(url, output, range: str, freq, duration, limit, num_thr
                 line = f"{metric_name}{metric_label_part} {value_to_use[1]} {timestamp}\n"
                 output_file.write(line)
 
-    process_metric(duration, freq, write_all_metrics_for_this_ts, True)
+        process_metric(duration, freq, write_all_metrics_for_this_ts)
 
-    # Don't have a \r\n, otherwise tsdb will crash with 'out of bounds'
-    output_file.write("# EOF\n")
-    output_file.close()
+        # Don't have a \r\n, otherwise tsdb will crash with 'out of bounds'
+        output_file.write("# EOF\n")
+        output_file.close()
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=16) as file_executor:
+        future_files = {file_executor.submit(write_single_file_for_metric, mn): mn for mn in metrics_dict.keys()}
+        for future in concurrent.futures.as_completed(future_files):
+            future.result()
+
+    # for metric_name in list(metrics_dict.keys()):
+
+
+@cli.command('import', help="Imports all the files (using tsdb import) created by the 'create' command")
+@click.option('-i', '--input', 'input_path', default="backfill_data", help='name of folder to use for created data')
+@click.option('-d', '--data', 'data_path', default="data", help='location of prometheus tsdb')
+@click.option('-c', '--done', 'done_path', default="backfill_done", help='where to move files when successfully imported')
+def import_from_folder(input_path, data_path, done_path):
+    if not os.path.exists(done_path):
+        os.mkdir(done_path)
+
+    for file in os.listdir(input_path):
+        file_name = os.path.join(input_path, file)
+        command = ["./tsdb", "import", file_name, data_path]
+        click.echo(f"Processing: {file}")
+        tsdb_run = subprocess.run(command)
+        if tsdb_run.returncode != 0:
+            raise click.ClickException(f"Failed to run with file: {file}, error: {tsdb_run.returncode}")
+        print(f"{file} OK. Result: {tsdb_run.returncode}.")
+        os.rename(file_name, os.path.join(done_path, file))
 
 
 if __name__ == '__main__':
